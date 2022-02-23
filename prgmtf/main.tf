36d22455413deb9d28b451532a63b02c4aa8ffe9
gky360 <gky360@gmail.com>
2022-02-23 20:18:14 +0900

    Update provider

--- prgmtf/main.tf
+++ prgmtf/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "~> 1.1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.2.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
