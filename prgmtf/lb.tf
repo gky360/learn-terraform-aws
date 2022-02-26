d339a186541dd4a79548e8a0a642cd71a64bb5a9
gky360 <gky360@gmail.com>
2022-02-26 17:59:10 +0900

    Add lb listenner

--- prgmtf/lb.tf
+++ prgmtf/lb.tf
@@ -108,3 +108,37 @@ resource "aws_acm_certificate_validation" "example" {
   certificate_arn         = aws_acm_certificate.example.arn
   validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
 }
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.example.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      concontent_type = "text/plain"
+      message_body    = "これは『HTTPS』です"
+      status_code     = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
