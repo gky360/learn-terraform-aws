e2f10468c2843667b001723e53da8615636bc9f6
gky360 <gky360@gmail.com>
2022-02-27 17:35:32 +0900

    Add lb listener rule

--- prgmtf/lb.tf
+++ prgmtf/lb.tf
@@ -164,3 +164,18 @@ resource "aws_lb_target_group" "example" {
 
   depends_on = [aws_lb.example]
 }
+
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    fixed  = "path-pattern"
+    values = ["/*"]
+  }
+}
