41854036ba37a0a5f6ec5e2a6ab734183df99942
gky360 <gky360@gmail.com>
2022-02-27 18:06:53 +0900

    Add ecs service

--- prgmtf/ecs.tf
+++ prgmtf/ecs.tf
@@ -1,4 +1,4 @@
-resource "aws_exs_cluster" "example" {
+resource "aws_ecs_cluster" "example" {
   name = "example"
 }
 
@@ -9,4 +9,42 @@ resource "aws_ecs_task_definition" "example" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./container_definitions.json")
+}
+
+resource "aws_ecs_service" "example" {
+  name                              = "example"
+  cluster                           = aws_ecs_cluster.example.arn
+  task_definition                   = aws_ecs_task_definition.example.arn
+  desired_count                     = 2
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.3.0"
+  health_check_grace_period_seconds = 60
+
+  network_configuration {
+    assignassign_public_ip = false
+    security_groups        = [module.nginx_sg.security_group_id]
+
+    subsubnets = [
+      aws_subnet.private_0.id,
+      aws_subnet.private_1.id,
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = "example"
+    container_port   = 80
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
+module "nginx_sg" {
+  source      = "./security_group"
+  name        = "nginx-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = [aws_vpc.example.cidr_block]
 }
