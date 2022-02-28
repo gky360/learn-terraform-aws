968357e7a50eebbc24438c9fb93812ef370f8877
gky360 <gky360@gmail.com>
2022-02-28 23:51:43 +0900

    Add ECS Scheduled Tasks definition

--- prgmtf/ecs.tf
+++ prgmtf/ecs.tf
@@ -75,3 +75,20 @@ module "ecs_task_execution_role" {
   identifier = "ecs-tasks.amazonaws.com"
   policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
+
+// ECS Scheduled Tasks
+
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name              = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
+
+resource "aws_ecs_task_definition" "example_batch" {
+  family                   = "example-batch"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./batch_container_definitions.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+}
