6471b9c1af6fbee90c804c754f8fcb8fc455ad9f
gky360 <gky360@gmail.com>
2022-03-01 00:59:59 +0900

    Add ssm parameter

--- prgmtf/ecs.tf
+++ prgmtf/ecs.tf
@@ -140,3 +140,21 @@ resource "aws_kms_alias" "example" {
   name          = "alias/example"
   target_key_id = aws_kms_key.example.key_id
 }
+
+resource "aws_ssm_parameter" "db_username" {
+  name        = "/db/username"
+  value       = "root"
+  type        = "String"
+  description = "データベースのユーザー名"
+}
+
+resource "aws_ssm_parameter" "db_password" {
+  name        = "/db/password"
+  value       = "uninitialized"
+  type        = "SecureString"
+  description = "データベースのパスワード"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
