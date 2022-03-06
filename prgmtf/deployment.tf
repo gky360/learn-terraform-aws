770ad61629599181363961dccc083bbed5ee99c7
gky360 <gky360@gmail.com>
2022-03-07 01:23:07 +0900

    Add GitHub webhook

--- prgmtf/deployment.tf
+++ prgmtf/deployment.tf
@@ -205,3 +205,20 @@ resource "aws_codepipeline_webhook" "example" {
     match_equals = "refs/heads/{Branch}"
   }
 }
+
+provider "github" {
+  organization = "your-github-name"
+}
+
+resource "github_repository_webhook" "example" {
+  repository = "your-repository"
+
+  configuration {
+    url          = aws_codepipeline_webhook.example.url
+    secret       = "VeryRandomStringMoreThan20Byte!"
+    content_type = "json"
+    insecure_ssl = false
+  }
+
+  events = ["push"]
+}
